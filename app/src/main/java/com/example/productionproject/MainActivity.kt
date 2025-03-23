d4a944cbb5d58dd436d6dba260d8ad6e325ee158
Jason Bartolome <J.Bartolome2432@student.leedsbeckett.ac.uk>
2025-03-23 23:03:42 +0000

    Added a BottomNavButton composable with icon placeholder to navigate the app

--- app/src/main/java/com/example/productionproject/MainActivity.kt
+++ app/src/main/java/com/example/productionproject/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -121,7 +122,20 @@ fun ExpenseInfo(
     }
 }
 
-
+@Composable
+fun BottomNavButton(icon: ImageVector, label: String) {
+    IconButton(onClick = { /* Add navigation logic here later */ }) {
+        Column(
+            horizontalAlignment = androidx.compose.ui.Alignment.CenterHorizontally
+        ) {
+            Icon(
+                imageVector = icon,
+                contentDescription = "$label Icon"
+            )
+            Text(text = label)
+        }
+    }
+}
 
 @Preview(showBackground = true)
 @Composable
